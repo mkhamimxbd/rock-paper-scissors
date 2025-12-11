d181005f2dda4f76db3b92294565d29cd021dc6d
M. K. Hamim <mkhamimbd@gmail.com>
2025-12-11 08:11:19 +0600

    changed back to the 'rock' 'paper' 'scissors' word from emojis

--- scripts/script.js
+++ scripts/script.js
@@ -11,30 +11,30 @@ function playGame(playerMove) {
 
   const computerMove = pickComputerMove();
 
-  if (playerMove === '✌️') {
+  if (playerMove === 'scissors') {
 
-    if (computerMove === '✊') {
+    if (computerMove === 'rock') {
       result = 'You lose.';
-    } else if (computerMove === '✋') {
+    } else if (computerMove === 'paper') {
       result = 'You won!';
-    } else if (computerMove === '✌️') {
+    } else if (computerMove === 'scissors') {
       result = 'Tie.';
     }
 
-  } else if (playerMove === '✋') {
-    if (computerMove === '✊') {
+  } else if (playerMove === 'paper') {
+    if (computerMove === 'rock') {
       result = 'You won!';
-    } else if (computerMove === '✋') {
+    } else if (computerMove === 'paper') {
       result = 'Tie.';
-    } else if (computerMove === '✌️') {
+    } else if (computerMove === 'scissors') {
       result = 'You lose.';
     }
-  } else if (playerMove === '✊') {
-    if (computerMove === '✊') {
+  } else if (playerMove === 'rock') {
+    if (computerMove === 'rock') {
       result = 'Tie.';
-    } else if (computerMove === '✋') {
+    } else if (computerMove === 'paper') {
       result = 'You lose.';
-    } else if (computerMove === '✌️') {
+    } else if (computerMove === 'scissors') {
       result = 'You won!';
     }
   }
@@ -68,11 +68,11 @@ function pickComputerMove() {
   let computerMove = '';
 
   if (randomNumber >= 0 && randomNumber < 1 / 3) {
-    computerMove = '✊';
+    computerMove = 'rock';
   } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-    computerMove = '✋';
+    computerMove = 'paper';
   } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-    computerMove = '✌️';
+    computerMove = 'scissors';
   }
 
   return computerMove;
