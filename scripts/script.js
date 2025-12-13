2be79ebd8d0753e8dc9ff97b3aaf61f7017e4d6e
M. K. Hamim <mkhamimbd@gmail.com>
2025-12-13 13:56:28 +0600

    added keydown for autoplay so if the key 'a' is pressed in the keyboard, autoplay will tregger

--- scripts/script.js
+++ scripts/script.js
@@ -98,6 +98,8 @@ document.body.addEventListener('keydown', (event) => {
     playGame('paper');
   } else if (event.key === 's') {
     playGame('scissors');
+  } else if (event.key === 'a') {
+    autoPlay();
   }
 });
 
